b8eb04590216c81a64b780accb77cb005ba5c99a
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-11-28 16:45:29 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,15 @@
 
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+manualReference = {}
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
+            manualReference[str({major} | {minor})] = i * 5 + j
     return len(major_colors) * len(minor_colors)
-
-
 result = print_color_map()
 assert(result == 25)
+assert(manualReference[str({"White"} | {"Blue"})] == 1)
+assert(manualReference[str({"Black"} | {"Green"})] == 12)
 print("All is well (maybe!)\n")
