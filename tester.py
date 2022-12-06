8fbf808d339ed3faab06d4f7510975c8a3981a3e
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-06 22:22:11 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,15 +1,13 @@
-from alerter import *
-alert_failure_count = 0
-alert_threshold = 200
-
+import alerter
+import configuration
 
 def testing():
-    alert_in_celcius(303.6)  
-    alert_in_celcius(392)    
-    alert_in_celcius(400.5)  
-    assert(alert_failure_count==1)
+    alerter.alert_in_celcius(303.6)  
+    alerter.alert_in_celcius(392)    
+    alerter.alert_in_celcius(400.5)  
+    assert(configuration.alert_failure_count==1)
     return "Success!"
 
      
 if __name__ == "__main__":
-    alerter.testing()
+   print("All is well")
