d8a538aac429e4dcda14ba6121686dc96b43b14c
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-06 22:36:43 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,7 +3,7 @@ import configuration
 
 def testing():
     alerter.alert_in_celcius(303.6)  
-    alerter.alert_in_celcius(392)    
+    alerter.alert_in_celcius(200)    
     alerter.alert_in_celcius(400.5)  
     assert(configuration.alert_failure_count==1)
     return "Success!"
