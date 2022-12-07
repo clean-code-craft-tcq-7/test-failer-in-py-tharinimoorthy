124f583ab31a96ad5842556feff8dfce18414015
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-07 09:48:20 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,14 +1,13 @@
 import alerter
 import configuration
 def testing():
-        alerter.alert_in_celcius(200)  
-        alerter.alert_in_celcius(300)    
-        alerter.alert_in_celcius(400)  
-        assert(configuration.alert_failure_count==1)
-        return "Success!"
+     alerter.alert_in_celcius(200)  
+     alerter.alert_in_celcius(300)    
+     alerter.alert_in_celcius(400)  
+     assert(configuration.alert_failure_count==1)
+     return "Success!"
 
-         
-    if __name__ == "__main__":
-        testing()
-        print("All is well") 
-    
+      
+if __name__ == "__main__":
+     testing()
+     print("All is well")    
